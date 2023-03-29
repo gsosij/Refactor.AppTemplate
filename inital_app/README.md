73f161e94538042eaa91e93040cfd469b339ebf8
Lee <lee_wallis@hotmail.co.uk>
2023-03-29 08:01:53 +0100

    Initial App

---
+++ inital_app/README.md
@@ -0,0 +1,3 @@
+# inital_app
+
+A new Flutter project.
---
+++ inital_app/android/app/src/main/kotlin/com/example/inital_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.inital_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
